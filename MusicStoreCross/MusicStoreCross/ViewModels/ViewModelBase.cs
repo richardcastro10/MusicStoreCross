c06f370550cd08738d8ca78b6e1d10ee5054060d
Verion <Verion@DESKTOP-LSCE307>
2024-02-06 14:03:45 -0300

    Adicionar arquivos de projeto.

---
+++ MusicStoreCross/MusicStoreCross/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace MusicStoreCross.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ MusicStoreCross/MusicStoreCross/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicStoreCross.Views
+{
+    public partial class MainView :  Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
